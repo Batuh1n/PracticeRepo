8c50a5c01d2ff1a25e1b68deb634c902c49db0a5
Batuh_n <batuhan.yayan1@gmail.com>
2025-02-06 00:20:52 +0300

    More than half of Level31 is done.

--- TheCsharpPlayersGuide/Level31FountainOfObjects.cs
+++ TheCsharpPlayersGuide/Level31FountainOfObjects.cs
@@ -1,13 +1,51 @@
+using System.Drawing;
+
 namespace ConsoleApp1;
 
+// February 4-6th, 2025
 public class TheFountainOfObjects
 {
     private Room[,] Grid { get; init; }
-
-    public TheFountainOfObjects(int GridWidth, int GridHeight)
+    private Point GridSize { get; }
+    public Room CurrentRoom
     {
-        Grid = new Room[GridWidth, GridHeight];
+        get => Grid[CurrentPosition.X, CurrentPosition.Y];
+        init => field = value;
     }
+
+    public Point CurrentPosition
+    {
+        get => field;
+        set
+        {
+            if (ValidMove(value)) field = value; // somehow you can't access field if you use "=>"
+            else Console.WriteLine("Can't move there.");
+        }
+    }
+
+    public TheFountainOfObjects(Point XYSize)
+    {
+        Grid = new Room[XYSize.X, XYSize.Y];
+        GridSize = XYSize;
+        CurrentRoom = Grid[0, 0];
+        CurrentPosition = new Point(0, 0);
+    }
+
+    public bool ValidMove(Point p)
+    {
+        bool isValid = false;
+        if (Math.Abs(CurrentPosition.X - p.X) == 1 && p.Y - CurrentPosition.Y == 0) isValid = true;
+        if (Math.Abs(CurrentPosition.Y - p.Y) == 1 && p.Y - CurrentPosition.Y == 0) isValid = true;
+        if (p.X > GridSize.X || p.Y > GridSize.Y) isValid = false;
+        return isValid;
+    }
+    
+
+    public void MoveNorth() => CurrentPosition = new Point(CurrentPosition.X, CurrentPosition.Y + 1);
+    public void MoveSouth() => CurrentPosition = new Point(CurrentPosition.X, CurrentPosition.Y - 1);
+    public void MoveEast() => CurrentPosition = new Point(CurrentPosition.X - 1, CurrentPosition.Y);
+    public void MoveWest() => CurrentPosition = new Point(CurrentPosition.X + 1, CurrentPosition.Y);
+    
 }
 
 public abstract class Room
@@ -40,14 +78,3 @@ public class FountainRoom : Room
     }
 }
 
-public class GridRooms // I'll probably delete this one
-{
-    public Room[,] Grid { get; init; }
-
-    GridRooms(int width, int height)
-    {
-        Grid = new Room[width, height];
-    }
-}
-
-public record struct Point(int X, int Y); // also this one
