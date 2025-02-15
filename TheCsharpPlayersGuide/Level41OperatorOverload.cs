63242d246b0b47820d8a2b82a93287a846319ce7
Batuh_n <batuhan.yayan1@gmail.com>
2025-02-16 00:58:22 +0300

    Level 42: Operator Overloads

--- TheCsharpPlayersGuide/Level41OperatorOverload.cs
+++ TheCsharpPlayersGuide/Level41OperatorOverload.cs
@@ -1,6 +1,39 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ConsoleApp1;
 
-public class Level41OperatorOverload
+public record BlockCoordinate(int Row, int Column)
 {
+    public static BlockCoordinate operator +(BlockCoordinate coord ,BlockOffset offset)
+        => new BlockCoordinate(coord.Row + offset.RowOffset, coord.Column + offset.ColumnOffset);
+    public static BlockCoordinate operator + (BlockOffset offset, BlockCoordinate coord)
+        => new BlockCoordinate(coord.Row + offset.RowOffset, coord.Column + offset.ColumnOffset);
+
+    public static BlockCoordinate operator +(BlockCoordinate coord, Direction direction)
+        => direction switch
+        {
+            Direction.East => new BlockCoordinate(coord.Row - 1, coord.Column),
+            Direction.West => new BlockCoordinate(coord.Row + 1, coord.Column),
+            Direction.North => new BlockCoordinate(coord.Row, coord.Column + 1),
+            Direction.South => new BlockCoordinate(coord.Row, coord.Column - 1)
+        };
     
+    public int this[int index]
+    {
+        get => index == 0 ? Row : index == 1 ? Column : throw new ArgumentOutOfRangeException(nameof(index));
+        // Could also use patterns here. index switch { 0 => Row, 1 => Column }
+    }
 }
+
+public record BlockOffset(int RowOffset, int ColumnOffset)
+{
+    public static implicit operator BlockOffset(Direction direction)
+        => direction switch
+        {
+            Direction.East => new BlockOffset(-1, 0),
+            Direction.West => new BlockOffset(1, 0),
+            Direction.North => new BlockOffset(0, 1),
+            Direction.South => new BlockOffset(0, -1),
+        };
+}
+public enum Direction { North, East, South, West }
