de13d4866ba65dedac16097e2192eab1a6a09c8e
Batuh_n <batuhan.yayan1@gmail.com>
2025-03-12 23:24:38 +0300

    multiple changes and additions

--- TheCsharpPlayersGuide/Level52TheFinalBattle.cs
+++ TheCsharpPlayersGuide/Level52TheFinalBattle.cs
@@ -10,13 +10,13 @@ public class GameRPG
     {
         
     }
-    public void Round()
+    public void Round() // TODO
     {
         foreach (ICharacter hero in Heroes)
         {
             Console.WriteLine($"It is {hero.Name}'s turn...");
             Thread.Sleep(ICharacter.Rand.Next(400, 1700));
-            DisplayInfo(hero.PickAction(Monsters));
+            //DisplayInfo(hero.PickAction(Monsters));
             Thread.Sleep(500);
             Console.WriteLine();
         }
@@ -25,27 +25,29 @@ public class GameRPG
         {
             Console.WriteLine($"It is {monster.Name}'s turn...");
             Thread.Sleep(ICharacter.Rand.Next(400, 1700));
-            DisplayInfo(monster.PickAction(Heroes));
+            //DisplayInfo(monster.PickAction(Heroes));
             Thread.Sleep(500);
             Console.WriteLine();
         }
     }
-
-    public void DisplayInfo(ActionResult result)
+    
+    
+    public void PartyInfo(ICharacter player)
     {
-        if (result.Actor == null || result.ActionName == null) throw new NotImplementedException();
-        string resultString = result switch
-        {
-            var z when z.Target == null => $"{z.Actor.Name} did {z.ActionName}.",
-            var z when z.ActionName == ActionTypes.SimpleAttack => $"{z.Actor.Name} attacked {z.Target}!",
-        };
-        Console.WriteLine(resultString);
+        if (Heroes.Contains(player)) 
+            foreach (ICharacter monster in Monsters) Console.WriteLine($"{Monsters.BinarySearch(monster)}: {monster.Name}" +
+                                                                       $"({monster.CurrentHP} HP)");
+
+        if (Monsters.Contains(player)) 
+            foreach(ICharacter hero in  Heroes) Console.WriteLine($"{Monsters.BinarySearch(hero)}: {hero.Name}" +
+                                                                  $"({hero.CurrentHP} HP)");
     }
 }
 public interface ICharacter
 {
     public static Random Rand { get; } = new Random();
-    int HP { get; }
+    public int MaxHP { get; init; }
+    int CurrentHP { get; }
     string Name { get; init; }
     Player Controller { get; init; }
 
@@ -53,31 +55,55 @@ public interface ICharacter
     public ActionResult PickAction(List<ICharacter> characters);
     public void TakeDamage(int damage);
 }
-public enum ActionTypes { Nothing, SimpleAttack}
+public enum ActionTypes { Nothing, Attack}
 
 public class TrueProgrammer : ICharacter
 {
-    public int HP { get; private set; } = 50;
+    public int MaxHP { get; init; } = 25;
+    public int CurrentHP { get; private set; } = 25;
     public string Name { get; init; }
     public Player Controller { get; init; }
+    
+    public TrueProgrammer(string name) => Name = name;
 
     public ActionResult PickAction(List<ICharacter> characters)
     {
-        return null;
+        ActionResult result = new() { Actor = this };
+        int x = Controller.ReturnInput(2, "0: Do nothing.\n1:Do a simple attack.");
+        switch (x)
+        {
+            case 0:
+                break;
+            case 1:
+                SimpleAttack(characters[ICharacter.Rand.Next(0, characters.Count)], result);
+                break;
+        }
+        return result;
     }
-    public void TakeDamage(int damage) => HP -= damage;
+    public void TakeDamage(int damage) => CurrentHP -= damage;
+
+    public void SimpleAttack(ICharacter character, ActionResult result)
+    {
+        result.ActionType = ActionTypes.Attack;
+        result.ActionName = "PUNCH";
+        result.Actor = character;
+        result.DamageDealt = 1;
+        character.TakeDamage(result.DamageDealt);
+    }
 }
 public class Skeleton : ICharacter
 {
-    public int HP { get; private set; } = 20;
+    public int MaxHP { get; init; } = 5;
+    public int CurrentHP { get; private set; } = 5;
     public string Name { get; init; } = "SKELETON";
     public Player Controller { get; init; }
 
     public Skeleton(Player controller) => Controller = controller;
+    
     public ActionResult PickAction(List<ICharacter> characters)
     {
         ActionResult result = new() { Actor = this };
-        int x = ICharacter.Rand.Next(0, 1);
+        int x = Controller.ReturnInput(2, "0: Do nothing.\n1:Do a simple attack.");
         switch (x)
         {
             case 0:
@@ -91,11 +117,13 @@ public class Skeleton : ICharacter
 
     public void SimpleAttack(ICharacter character, ActionResult result)
     {
-        result.ActionName = ActionTypes.SimpleAttack;
+        result.ActionType = ActionTypes.Attack;
+        result.ActionName = "BONECRUNCH";
         result.Actor = character;
-        character.TakeDamage(2);
+        result.DamageDealt = ICharacter.Rand.Next(0, 1);
+        character.TakeDamage(result.DamageDealt);
     } 
-    public void TakeDamage(int damage) => HP -= damage;
+    public void TakeDamage(int damage) => CurrentHP -= damage;
 }
 
 public class Player
@@ -105,15 +133,15 @@ public class Player
     public Player (bool botControlled) => BotControlled = botControlled;
     public int ReturnInput(uint amountOfChoices, string choiceInfo = null)
     {
-        if (BotControlled) return ICharacter.Rand.Next(0, (int)amountOfChoices);
+        if (BotControlled) return ICharacter.Rand.Next(1, (int)amountOfChoices);
         Console.WriteLine(choiceInfo);
-        int choiceIpnut = Convert.ToInt32(Console.ReadLine());
-        while (!(choiceIpnut <= amountOfChoices && choiceIpnut > 0))
+        int choiceInput = Convert.ToInt32(Console.ReadLine());
+        while (!(choiceInput <= amountOfChoices && choiceInput > 0))
         {
             Console.WriteLine("Not a valid input!");
-            choiceIpnut = Convert.ToInt32(Console.ReadLine());
+            choiceInput = Convert.ToInt32(Console.ReadLine());
         }
-        return choiceIpnut;
+        return choiceInput;
     }
 }
 
@@ -121,5 +149,15 @@ public class ActionResult
 {
     public ICharacter? Actor { get; set; }
     public ICharacter? Target { get; set; }
-    public ActionTypes ActionName { get; set; } = ActionTypes.Nothing;
+    public ActionTypes ActionType { get; set; } = ActionTypes.Nothing;
+    public string ActionName { get; set; } = "NOTHING";
+    public int DamageDealt { get; set; }
+
+    public void DisplayInfo()
+    {
+        if (Actor == null || ActionType == null) throw new NotImplementedException();
+        if (Target == null) Console.WriteLine($"{Actor.Name} used {ActionType}.");
+        else  Console.WriteLine($"{Actor.Name} used {ActionType} on {Target.Name}! ({DamageDealt} damage dealth --> " +
+                                $"{Target.CurrentHP}/{Target.MaxHP})");
+    }
 }
